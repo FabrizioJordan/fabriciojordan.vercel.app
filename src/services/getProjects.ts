bbbb99bca0d55fa165a9f7e29d2c429e11799fd4
FabricioJordan <fabriorange28@gmail.com>
2024-06-26 00:43:24 -0300

    "Removed Card component and updated Layout, index, and styles to reflect new design and functionality. All web copied from another mine repo."

---
+++ src/services/getProjects.ts
@@ -0,0 +1,46 @@
+
+
+
+
+
+/* .then(datas => iterPuts(datas));
+
+// fetcheando el json para agarrar los datos
+
+const data = await fetch("C:\Users\famjo\Downloads\FJ\Git\astro-portfolio\src\services\docs\projects.json")
+  .then(res => res.json());
+
+
+  //      ---->     { variable que se cambia en caso de poner mas o menos celuares  }
+var cantidadPhones = 6;
+
+function Card({ id, name, image, url, github }) {
+  try {
+    return `
+    
+  `;
+  }
+  catch (error) {
+    console.error("Error al retornar en la funciona Card, error ->  " + error)
+  }
+}
+
+// PUT PHONES : PONE LOS CELULARES
+function putPhones(phone) {
+  try {
+    document.getElementById("app").innerHTML += Card(phone);
+  } catch (error) {
+    console.log(`Error en el ${phone}, el error: + ${error}`);
+  }
+}
+
+// AQUI LE HACEMOS UN FOR A LOS PHONES PARA LLAMAR A LA FUNCION PUTPHONES DEPENDIENDO DE CUANTOS PHONES HAY
+function iterPuts(phones) {
+  for (var i = 0; i < phones.length; i++) {
+    putPhones(phones[i])
+  }
+}
+
+
+
+*/
