6c8eb603c0db31aa779972babe1556ef7038ddd0
FabricioJordan <fabriorange28@gmail.com>
2024-06-27 00:26:43 -0300

    Corrected MainProjects. All of SVGs filled in white. All no used images, SVGs and files deleted.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			transitionProperty:{
+				'opacityTransform': 'opacity, transform',
+				'filterColors': 'colors, filter, transform'
+			}
+		},
 	},
 	plugins: [],
 }
