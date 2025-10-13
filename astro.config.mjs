7b5fbeb1e2d8c294f02cb53f36f727262625fc32
Fabricio Jordán <dev.fjordan@gmail.com>
2025-10-13 01:45:13 -0300

    refactor(config): Migrate relative paths to @ alias on all the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  alias: {
+    '@layouts': './src/layouts',
+    "@MainLayout": ["src/layouts/Layout.astro"],
+    '@components': './src/components',
+    '@pages': './src/pages',
+    '@assets': './src/assets',
+    "@services/*": ["src/services/*"]
+  }
 });
